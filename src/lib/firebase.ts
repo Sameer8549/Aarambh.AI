e5fbed9e11bd2658b7b43ec0bc667a9fc14acc2a
Aiskillshouse <aiskillshouse@gmail.com>
2025-09-01 10:57:20 +0000

    AIzaSyD6iawfGPy2UMAuqWdwhO9i8dtnWLbjH1k AIzaSyDIGi0UcVUyd3NLGXYVsMpJzWOz

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,7 +6,7 @@ const firebaseConfig = {
   projectId: "aarambhai-youth-wellness",
   appId: "1:552156856845:web:295aa03f56fe6a82b50b44",
   storageBucket: "aarambhai-youth-wellness.firebasestorage.app",
-  apiKey: "AIzaSyAOq08ykTrFQY9lvCUrD6i5GwszFCkBRZ0",
+  apiKey: "AIzaSyD6iawfGPy2UMAuqWdwhO9i8dtnWLbjH1k",
   authDomain: "aarambhai-youth-wellness.firebaseapp.com",
   messagingSenderId: "552156856845",
 };
