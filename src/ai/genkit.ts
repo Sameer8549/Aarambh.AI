9c51ca209852a9b9b5bf0396dce9bcd9e3abee6b
Aiskillshouse <aiskillshouse@gmail.com>
2025-09-14 04:16:21 +0000

    Error: [GoogleGenerativeAI Error]: Error fetching from https://generativ

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,6 +2,6 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [googleAI({apiKey: "AIzaSyAoy71Bilgih8-Z_8CKeCw79JlIV1adSpY"})],
   model: 'googleai/gemini-1.5-flash',
 });
